1a72a90e765be4324d9cf15b65c0278be042db3b
ramunettam <ramunettam@gmail.com>
2024-04-19 12:37:06 +0200

    shell comiit

--- 04-array.sh
+++ 04-array.sh
@@ -2,7 +2,7 @@
 
 MOVIES=("rr" "dj" "bb" )
 
-echo "first movie::${MOVIES[0]}
+echo "first movie: ${MOVIES[0]}"
 
-echo "second movie${MOVIES[1]}
+echo "second movie:${MOVIES[1]}"
 echo "all movies${MOVIES[@]}"
