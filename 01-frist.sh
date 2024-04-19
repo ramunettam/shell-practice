c38e9d3bf7d257e2e10aad8a0d38b52817fce83f
ramunettam <ramunettam@gmail.com>
2024-04-19 12:09:18 +0200

    shell comiit

--- 01-frist.sh
+++ 01-frist.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-person1=ramu
-person2=krish
 
 echo "ramu:: hi krish, how are you?"
 echo "krish:: hi ram , iam good"
---
+++ 02-vaiable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=ramu
+person2=krish
+
+echo "$person1: hi $persons2, how are you?"
+echo "$person2:: hi $person1 , iam good"
+echo "$person: what are you doing"
+echo "$person2 :: i working"
