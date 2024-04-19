9614b3c3ad39f51837ebc50dd19bab136bb6bb1a
ramunettam <ramunettam@gmail.com>
2024-04-19 12:20:33 +0200

    shell comiit

---
+++ 03-parameter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "enter person1 name"
+read person1
+echo "person2 name" 
+read person2
+echo "$person1: hi $persons2, how are you?"
+echo "$person2:: hi $person1 , iam good"
+echo "$person: what are you doing"
+echo "$person2 :: i working"
