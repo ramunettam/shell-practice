5a7142d4291e965c843fc0d63c2bf6d6d4802d87
ramunettam <ramunettam@gmail.com>
2024-04-19 12:45:10 +0200

    shell comiit

--- 04-array.sh
+++ 04-array.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 MOVIES=("rr" "dj" "bb" )
+r=\e[1;31m
+Y=\e[1;34m
+n=\e[1;0m
+t=\e[1;32m
 
-echo "first movie: ${MOVIES[0]}"
+echo -e " $r first movie: ${MOVIES[0]} $n"
 
-echo "second movie:${MOVIES[1]}"
-echo "all movies:${MOVIES[@]}"
+echo -e  "$y second movie:${MOVIES[1]}$n"
+echo -e "$t all movies: ${MOVIES[@]}$n"
