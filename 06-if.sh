98c6fdcb31359dc28392a263a54793347818c1c1
ramunettam <ramunettam@gmail.com>
2024-04-19 13:17:26 +0200

    shell comiit

---
+++ 06-if.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read num1
+
+echo "enter num to check it is greate than 30"
+
+if[$num -gt 30]
+then
+echo "the given num  $nun is greater than 30"
+else
+echo "given num is less tan 30"
+fi
